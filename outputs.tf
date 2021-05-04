f2cf46e750af9bf70165cbdb63d5489e8d7f688f
rb-nr <59568085+rb-nr@users.noreply.github.com>
2021-05-04 10:32:28 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "json_map" {
   description = "Map type of container definition"
-  value       = jsondecode(module.container.json_map)
+  value       = jsondecode(module.container.json_map_encoded)
 }
 
 output "json" {
   description = "String type of container definition"
-  value       = module.container.json_map
+  value       = module.container.json_map_encoded
 }
